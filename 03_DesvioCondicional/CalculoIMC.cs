9781385734408713f5620062492766da047618d1
CAUA ALVES DOS SANTOS <caua.asantos2@senacsp.edu.br>
2025-08-07 19:59:17 -0300

    Calculo de IMC em C#

--- 03_DesvioCondicional/CalculoIMC.cs
+++ 03_DesvioCondicional/CalculoIMC.cs
@@ -6,16 +6,16 @@ class CalculoIMC
 	{
 		double peso, altura, imc; //variaveis do tipo "double"	
 		
-		Console.WriteLine("Digite seu peso (kg): "); //Console.WriteLine()escreve na tela
-		//Console.ReadLine() é equivalente ao leia do Portugol
+		Console.WriteLine("Digite seu peso (kg): "); //"Console.WriteLine()" - escreve na tela
+		//"Console.ReadLine()" - é equivalente ao leia do Portugol
 		
-		peso = Double.Parse( Console.ReadLine() ); //"parse()" converte o texto para o número
+		peso = Double.Parse( Console.ReadLine() ); //Variavel com o  ".parse()" - converte o texto para o número
 		
 		Console.WriteLine("Digite sua altura (m): ");
-		//Double.Parse() coverte o texto em número decimal
+		//"Double.Parse()" - coverte o texto em número decimal
 		altura = double.Parse( Console.ReadLine() );
 		
-		imc = peso / (altura * altura);
+		imc = peso / (altura * altura); //Caclculo do IMC
 		
 		Console.WriteLine("Seu IMC é {0:F3}", imc );
 		
