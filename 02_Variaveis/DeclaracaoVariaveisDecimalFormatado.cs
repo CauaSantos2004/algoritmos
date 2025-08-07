ce3f41089612d8bae07fe61fd2f80fceec627a5f
CAUA ALVES DOS SANTOS <caua.asantos2@senacsp.edu.br>
2025-08-07 19:55:52 -0300

    Criação do Código C# para demonstrar textos com Placeholder e valores formatados

--- 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
+++ 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
@@ -7,7 +7,8 @@ class DeclaracaovariaveisDecimalFormatado
 		decimal x = 0.999m;
 		decimal y = 99999999999999999999m;
 		
-		/*Utilizando o Placeholder(posição de marcação) para demonstrar 
+		/*
+		Utilizando o Placeholder(posição de marcação) para demonstrar 
 		o conteúdo de uma variavel no texto
 		{0:C} 
 		0 = posição de marcação
@@ -15,7 +16,15 @@ class DeclaracaovariaveisDecimalFormatado
 		*/
 		Console.WriteLine("Minha quantia - {0:C}", x);
 		Console.WriteLine("Sua quantia - {0:C}", y);
-		
 		Console.WriteLine("Valor de x = {0: F3} e Valor de y = {1:C}",x,y);
+	
+		/*
+		Interpolação de Sting, só funciona no .Net versão 6.0 acima
+		*/
+		/*
+		Console.WriteLine($"Minha quantia - {0:C}");
+		Console.WriteLine($"Sua quantia - {0:C}");
+		Console.WriteLine($"Valor de x = {0: F3} e Valor de y = {y:C}");
+		*/
 	}
 }
