2826e3fa4e1f034b5083fd33cafe1a12c32899e1
caua.asantos2 <caua.santos.devs@gmail.com>
2025-08-22 21:57:53 -0300

    Código de Criação de Calendários.

---
+++ 09_Calendario/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Calendario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o mês (1..12)");
+            int mes = int.Parse( Console.ReadLine() );
+
+            Console.Write("Digite o ano: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            //Descobre a quantidade de dias de um mês (mês que o usúario digitou)
+            int diasDoMes = DateTime.DaysInMonth(ano, mes);
+
+            //Descobre o dia da semana do primeiro dia do mês
+            // 0 = Domingo - 6 = Sábado
+            //Gera o primeiro dia do mês e ano informado pelo usúario
+            DateTime primeiroDiaMes = new DateTime(ano, mes, 1);
+            int diaSemanaInicio = (int)primeiroDiaMes.DayOfWeek;
+
+            //Matriz de 6 semanas e 7 dias
+            int[,] calendario = new int[6,7];
+
+            int dia = 1;
+
+            //Preenche a matriz com os dois dias do mês
+            for (int semana = 0; semana < 6; semana++)
+            {
+                for (int diaSemana = 0; diaSemana < 7; diaSemana++)
+                {
+                    if (semana == 0 && diaSemana < diaSemanaInicio)
+                    {
+                        calendario[semana, diaSemana] = 0;
+                    }
+                    else if(dia <= diasDoMes)
+                    {
+                        calendario[semana, diaSemana] = dia;
+                        dia++;
+                    }
+                }
+            }
+            Console.WriteLine($"\nCalendário de " +
+                $"{primeiroDiaMes.ToString("MMMM")} de {ano}");
+
+            Console.WriteLine("DOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB");
+
+            //impressão do calendário
+            for (int semana = 0; semana < 6; semana++)
+            {
+                for (int diaSemana = 0; diaSemana < 7; diaSemana++)
+                {
+                    if (calendario[semana, diaSemana] != 0)
+                    {
+                        Console.Write(calendario[semana, diaSemana].ToString("D2") + "\t");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+                }
+                Console.WriteLine(); // nova linha por semana
+            }
+                Console.WriteLine();
+            //Espera o usúario teclar qualquer tecla
+            Console.ReadKey();
+        } 
+                
+    }   
+
+}
